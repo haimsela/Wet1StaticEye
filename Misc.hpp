850e8b4af120d06f51bb6633f6c01cff32721b05
guynimri <guynimri@technion.campus.ac.il>
2018-12-12 14:15:28 +0200

    mess

---
+++ Misc.hpp
@@ -0,0 +1,11 @@
+#pragma once
+#include "library2.h"
+#include  <new>
+#include <iostream>
+
+/*	Add your includes here:
+		They should contain:
+		1.Enum StatusType definition.
+		2.All the provided functions definitions:
+		(Init,AddImage,DeleteImage,AddLabel,DeleteLabel etc...)
+*/
